6f78545621d175b2a8a37319e7a39e0c42fd0dbc
mert baytas <mertbaytas@gmail.com>
2023-07-07 17:15:48 +0300

    finished version

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
-//başla a
+//başla
 class _MyAppState extends State<MyApp> {
 var response;
 var parsedResponse;
